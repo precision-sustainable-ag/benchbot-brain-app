ed2a1168acda0560b2794ef2a47028e95c8940ee
Priya Jakhar <priyaftmage@gmail.com>
2024-01-19 16:40:29 -0500

    add camera library files

---
+++ mini_comp_api/camera_sdk/build/CMakeFiles/RemoteCli.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/benchbot/camera/app/CameraDevice.cpp" "CMakeFiles/RemoteCli.dir/app/CameraDevice.cpp.o" "gcc" "CMakeFiles/RemoteCli.dir/app/CameraDevice.cpp.o.d"
+  "/home/benchbot/camera/app/ConnectionInfo.cpp" "CMakeFiles/RemoteCli.dir/app/ConnectionInfo.cpp.o" "gcc" "CMakeFiles/RemoteCli.dir/app/ConnectionInfo.cpp.o.d"
+  "/home/benchbot/camera/app/MessageDefine.cpp" "CMakeFiles/RemoteCli.dir/app/MessageDefine.cpp.o" "gcc" "CMakeFiles/RemoteCli.dir/app/MessageDefine.cpp.o.d"
+  "/home/benchbot/camera/app/PropertyValueTable.cpp" "CMakeFiles/RemoteCli.dir/app/PropertyValueTable.cpp.o" "gcc" "CMakeFiles/RemoteCli.dir/app/PropertyValueTable.cpp.o.d"
+  "/home/benchbot/camera/app/RemoteCli.cpp" "CMakeFiles/RemoteCli.dir/app/RemoteCli.cpp.o" "gcc" "CMakeFiles/RemoteCli.dir/app/RemoteCli.cpp.o.d"
+  "/home/benchbot/camera/app/Text.cpp" "CMakeFiles/RemoteCli.dir/app/Text.cpp.o" "gcc" "CMakeFiles/RemoteCli.dir/app/Text.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")

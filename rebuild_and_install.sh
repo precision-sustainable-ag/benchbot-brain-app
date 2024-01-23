056e62cb90f15f70bfc1277088b01a8507ac67e7
mikahpinegar <55995241+mikahpinegar@users.noreply.github.com>
2024-01-23 17:46:25 +0000

    remove install phase

--- rebuild_and_install.sh
+++ rebuild_and_install.sh
@@ -9,11 +9,3 @@ cd ../api/
 source venv/bin/activate
 pip install -r requirements.txt
 python main.py
-
-# install the app on the brain, this may be unneeded
-set -uxeo pipefail
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-rm -f ~/manifest.json
-ln -s "$DIR/manifest.json" ~/manifest.json

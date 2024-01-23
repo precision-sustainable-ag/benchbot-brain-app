672a68e7957abc83e039258c322ff68c065dc193
mikahpinegar <55995241+mikahpinegar@users.noreply.github.com>
2024-01-23 18:15:12 +0000

    add install

--- rebuild_and_install.sh
+++ rebuild_and_install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash -x
 
+# install the app on the brain
+set -uxeo pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+rm -f ~/manifest.json
+ln -s "$DIR/manifest.json" ~/manifest.json
+
 # build the frontend in the ts directory
 cd ts/
 npm run build

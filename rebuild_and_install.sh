3eead53886ec578d2400dd7de1b4f2269bfbec74
mikahpinegar <55995241+mikahpinegar@users.noreply.github.com>
2024-01-23 18:03:24 +0000

    fix env location

--- rebuild_and_install.sh
+++ rebuild_and_install.sh
@@ -5,7 +5,8 @@ cd ts/
 npm run build
 
 # activate venv, install requirements, and run main.py 
-cd ../api/
+cd ../
 source venv/bin/activate
+cd ./api
 pip install -r requirements.txt
 python main.py

168930bf95b5b1d51dbf10eef30ddafec3716546
Priya Jakhar <priyaftmage@gmail.com>
2024-01-26 11:49:00 -0500

    add motor motion functions
    functions to move the x and z motors moved to class

--- brain_api/common/motor_controller.py
+++ brain_api/common/motor_controller.py
@@ -3,13 +3,18 @@ import socket
 from from_root import from_here
 
 
-class UDP_CONN():
+class MotorController():
     def __init__(self):
+        # x axis -> 0.003175 cm per enocoder count
+        # z axis -> 0.00529167 cm per encoder count
+        self.x_steps_to_cm = 0.003175
+        self.z_steps_to_cm = 0.000529167
+
         self.config_file = from_here('udp_config.json')
         with open(self.config_file, 'r') as openfile:
             udp_info = json.load(openfile)
-        self.ip = udp_info["ip"]
-        self.port = udp_info["port"]
+        self.ip = udp_info.get("ip")
+        self.port = udp_info.get("port")
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.init_connection()
  
@@ -42,3 +47,17 @@ class UDP_CONN():
         except:
             msg_reply = "Error! No reply from server"
         return msg_reply
+    
+    def move_motors(self, x_val, z_val):
+        x_counts = int(x_val) // self.x_steps_to_cm
+        z_counts = int(z_val) // self.z_steps_to_cm
+        message = f"X:{x_counts} Z:{z_counts}"
+        return self.send_message(message)
+
+    def homing_x(self):
+        message = "X:999 Z:0"
+        return self.send_message(message)
+    
+    def homing_z(self):
+        message = "X:0 Z:999"
+        return self.send_message(message)
--- brain_api/main.py
+++ brain_api/main.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from pathlib import Path
 from from_root import from_root, from_here
 from common.motors import Motors
-from common.udp_conn import UDP_CONN
+from common.motor_controller import MotorController
 import uvicorn
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
 app = FastAPI()
@@ -17,7 +17,7 @@ app.add_middleware(
 )
 
 amiga_motors = Motors()
-clear_core = UDP_CONN()
+clear_core = MotorController()
 
 
 @app.get("/move_yaxis/{dist}")
@@ -31,28 +31,17 @@ async def move_yaxis(dist):
 # +z is down, -z is up
 @app.get("/clearcore")
 def move_xz_axis(x, z):
-    # x axis -> 0.003175 cm per enocoder count
-    # z axis -> 0.00529167 cm per encoder count
-    x_steps_to_cm = 0.003175
-    z_steps_to_cm = 0.000529167
-
-    x_counts = int(x) // x_steps_to_cm
-    z_counts = int(z) // z_steps_to_cm
-
-    message = f"X:{x_counts} Z:{z_counts}"
-    return clear_core.send_message(message)
+    return clear_core.move_motors(x, z)
 
 
 @app.get("/home_x")
 def home_x():
-    message = "X:999 Z:0"
-    return clear_core.send_message(message)
+    return clear_core.homing_x()
 
 
 @app.get("/home_z")
 def home_z():
-    message = "X:0 Z:999"
-    return clear_core.send_message(message)
+    return clear_core.homing_z()
 
 
 @app.get("/udp_update")

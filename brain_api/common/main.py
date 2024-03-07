29f39d0a92c067d8c794de7144040fea8bf914c3
Priya Jakhar <priyaftmage@gmail.com>
2024-03-07 10:39:08 -0500

    add class for building paths/tracks

--- brain_api/common/main.py
+++ brain_api/common/main.py
@@ -3,9 +3,12 @@ import asyncio
 import time
 from pathlib import Path
 from motor_controller_y2 import MotorController_Y
+from path_builder import PathBuilder
 
 
 y_motor_control = MotorController_Y(1, 0.1)
+
+path_constructor = PathBuilder()
 
 # async def move_y_axis():
 #     await y_motor_control.run()
@@ -15,6 +18,7 @@ def move_y_axis():
     
 if __name__ == "__main__":
 
-    move_y_axis()
+    # move_y_axis()
+    path_constructor.build_path(1, 0.01)
     
 

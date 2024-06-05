8c5c950cb90a5a6577b5202692a07a85748e81ee
Priya Jakhar <priyaftmage@gmail.com>
2024-06-05 19:47:38 -0400

    add files for auto transfer of images

--- mini_computer_api/data_transfer.sh
+++ mini_computer_api/data_transfer.sh
@@ -1,28 +1,41 @@
 #!/bin/bash
 
-date=$(date +"%Y-%m-%d")
-path_src=/home/benchbot/benchbot-brain-app/mini_computer_api/images/$date
+mount_point=/home/benchbot/backup/
+if [[ ! -e $mount_point ]]; then
+    mkdir -p "$mount_point"
+fi
 
-path_dst=/home/benchbot/
-if [[ ! -e $path_dst ]]; then
-    mkdir -p "$path_dst"
-
-status_file=files_copied.txt
+status_file=/home/benchbot/files_copied.txt
 if [ -f "$status_file" ]; then
     :
 else
     touch "$status_file"
 fi
 
+sleep 5
+sudo mount /dev/sda $mount_point
+sleep 1
+
+date=$(date +"%Y-%m-%d")
+path_src=/home/benchbot/benchbot-brain-app/mini_computer_api/images/$date
+path_dst=$mount_point/$date
+if [[ ! -e $path_dst ]]; then
+    mkdir -p "$path_dst"
+fi
+
 for img_file in $path_src/*
 do
     bname=$(basename "$img_file")
     dst_file=$path_dst/"$bname"
-
     if [ -f "$dst_file" ]; then
         :
-    else 
-        cp -u "$img_file" "$dst_file"
-        echo "$bname" >> "$status_file"
+    else
+        if cp -u "$img_file" "$dst_file"
+        then echo "$img_file to $dst_file" >> "$status_file"
+        fi
     fi
 done
+
+echo "File Transfer Done"
+sleep 1
+umount /dev/sda
---
+++ mini_computer_api/init_transfer.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo /home/benchbot/benchbot-brain-app/mini_computer_api/data_transfer.sh | at now

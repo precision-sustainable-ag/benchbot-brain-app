61a7ecc157a66dff7476e3d8ffc90f4c23659f72
Priya Jakhar <priyaftmage@gmail.com>
2024-06-14 10:36:31 -0400

    cleanup logging and add break statement

--- mini_computer_api/delete_uploaded_images.sh
+++ mini_computer_api/delete_uploaded_images.sh
@@ -12,7 +12,6 @@ data_age_limit=1
 for i in $(globus ls "ea8aff4a-274e-4c48-bc23-5b93da0cc941:/semifield-upload/"); do
     # if folder does not exist locally, continue
     if [ ! -d "/home/benchbot/benchbot-brain-app/mini_computer_api/images/${i::-1}" ]; then
-      echo "/home/benchbot/benchbot-brain-app/mini_computer_api/images/${i::-1} does not exist."
       continue
     fi
 
@@ -22,20 +21,15 @@ for i in $(globus ls "ea8aff4a-274e-4c48-bc23-5b93da0cc941:/semifield-upload/");
 
     # create a date for $data_age_limit days ago
     limit_days_ago=$(date +%F -d "$data_age_limit days ago")
-    
-    # echo out date and limit_days_ago
-    echo "date: $date limit: $limit_days_ago"
 
     # if folder date is older than $data_age_limit, fetch all files in the directory
     if [[ $date -ge $limit_days_ago ]]
     then
-        echo "Date is older than $data_age_limit days"
+        echo "Folder date: $date is $data_age_limit days old or older"
 
         # assume folder has all images until proved otherwise
         folder_missing_file=false
         files_uploaded_to_globus=$(globus ls "ea8aff4a-274e-4c48-bc23-5b93da0cc941:/semifield-upload/$i")
-
-        echo $files_uploaded_to_globus
 
         # pipe globus ls of all files into files_uploaded_to_globus.txt
         echo "$files_uploaded_to_globus" > files_uploaded_to_globus.txt
@@ -48,13 +42,11 @@ for i in $(globus ls "ea8aff4a-274e-4c48-bc23-5b93da0cc941:/semifield-upload/");
           # grep files_uploaded_to_globus.txt for the current file, if it exists set current_image_missing to false, else true
           grep -qrn "$j" "files_uploaded_to_globus.txt" && current_image_missing=false || current_image_missing=true
 
-          # echo current_image_missing
-          echo $current_image_missing
-
           if [ "$current_image_missing" = true ] ; then
             # if any file is missing, set folder_missing_file to true
             echo "folder missing files!"
             folder_missing_file=true
+            break
           fi
         done
 
@@ -63,6 +55,7 @@ for i in $(globus ls "ea8aff4a-274e-4c48-bc23-5b93da0cc941:/semifield-upload/");
           echo "Some files are not uploaded!"
         # else, all files are uploaded, delete folder
         else
+          echo "deleting file: /home/benchbot/benchbot-brain-app/mini_computer_api/images/$i"
           echo "$i" >> ./logs/deleted_files.log 
           echo $(ls "/home/benchbot/benchbot-brain-app/mini_computer_api/images/$i") >> ./logs/deleted_files.log
           echo $(rm -r "/home/benchbot/benchbot-brain-app/mini_computer_api/images/$i")

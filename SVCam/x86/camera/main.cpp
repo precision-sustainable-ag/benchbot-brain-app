44a25d5488b2d5393124a5d8e0cc9793a5cf27cb
Priya Jakhar <priyaftmage@gmail.com>
2024-08-09 15:13:07 -0400

    fix files in camera dir

--- SVCam/x86/camera/main.cpp
+++ SVCam/x86/camera/main.cpp
@@ -10,9 +10,6 @@
 using namespace std;
 using namespace std::chrono;
 
-#include <pybind11/pybind11.h>
-namespace py = pybind11;
-
 
 bool InitSDK()
 {
@@ -83,23 +80,4 @@ int main(int argc, char**argv){
         sv_cam.disconnectCamera();
     }
     return 0;
-}
-
-
-PYBIND11_MODULE(SVCam, m) {
-    // optional module docstring
-    m.doc() = "Python binding for camera SDK";
-
-    m.def("InitSDK", &InitSDK, "function for initializing camera SDK");
-    m.def("findSystem", &findSystem, "function for finding the system for the camera");
-
-    py::class_<Camera>(m, "Camera")
-        .def(py::init<SV_SYSTEM_HANDLE>())
-        .def("deviceDiscovery", &Camera::deviceDiscovery)
-        .def("connectCamera", &Camera::connectCamera)
-        .def("disconnectCamera", &Camera::disconnectCamera);
-        // .def("openStream", &Camera::openStream)
-        // .def("startAcquisition", &Camera::startAcquisition)
-        // .def("stopAcquisition", &Camera::stopAcquisition)
-        
 }

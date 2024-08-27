ddd2b93dac1c2788051e897332201fc83e001af4
Priya Jakhar <priyaftmage@gmail.com>
2024-08-27 14:48:38 -0400

    add files to use with pybind

---
+++ mini_computer_api/camera_sdk/pybinding/RemoteCli.cpp
@@ -0,0 +1,83 @@
+﻿#include <cstdint>
+#include <filesystem>
+namespace fs = std::filesystem;
+#include <cstdlib>
+#include <iomanip>
+#include "CRSDK/CameraRemote_SDK.h"
+#include "CameraDevice.h"
+#include "Text.h"
+#include <fstream>
+#include <thread>
+#include <chrono>
+
+using namespace std::chrono_literals;
+namespace SDK = SCRSDK;
+
+
+class Sony_Camera{
+private:
+	typedef std::shared_ptr<cli::CameraDevice> CameraDevicePtr;
+	CameraDevicePtr camera;
+
+public:
+	bool init_sdk(){
+		auto init_success = SDK::Init();
+		if (!init_success) {
+			SDK::Release();
+			return false;
+		}
+		return true;
+	}
+
+	bool connect_camera(){
+		SDK::ICrEnumCameraObjectInfo* camera_list = nullptr;
+		auto enum_status = SDK::EnumCameraObjects(&camera_list);
+		if (CR_FAILED(enum_status) || camera_list == nullptr) {
+			SDK::Release();
+			return false;
+		}
+		auto* camera_info = camera_list->GetCameraObjectInfo(0);
+		camera = CameraDevicePtr(new cli::CameraDevice(1, camera_info));
+		return true;
+	}
+
+	void click_picture(){
+		camera->connect(SDK::CrSdkControlMode_Remote, SDK::CrReconnecting_ON);
+		camera->af_shutter();
+		std::this_thread::sleep_for(5s);
+	}
+
+	void disconnect_camera(){
+		camera->disconnect();
+		camera->release();
+		SDK::Release();
+	}
+}
+
+int main()
+{
+	bool success;
+	Sony_Camera cam_obj();
+	
+	success = cam_obj.init_sdk();
+	if (!success)
+		tout << std::endl << "Failed to initialize SDK\n";
+	// success = NULL;
+
+	success = cam_obj.connect_camera();
+	if (!success)
+		tout << std::endl << "Failed to connect to a camera\n";
+	else
+		cam_obj.click_picture();
+	
+	tout << std::endl << "Will disconnect soon\n";
+	std::this_thread::sleep_for(2s);
+	cam_obj.disconnect_camera();
+	
+	// Sony_Camera cam_obj();
+	// cam_obj.init_sdk();
+	// cam_obj.connect_camera();
+	// cam_obj.click_picture();
+	// std::this_thread::sleep_for(2s);
+	// cam_obj.disconnect_camera();
+}

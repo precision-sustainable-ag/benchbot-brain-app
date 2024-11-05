cd9f1ff5470502d3f55fdbf8e52f6fa46a29121e
Priya Jakhar <priyaftmage@gmail.com>
2024-11-05 17:21:06 -0500

    take out logging
    - comment timer related code
    - remove commented code

--- mini_computer_api/resources/camera_controller.py
+++ mini_computer_api/resources/camera_controller.py
@@ -7,17 +7,12 @@ import os
 import time
 import shutil
 import threading
-import logging
 import glob
 import cv2
 import io
 import yaml
 import json
 import resources.SVCam as SVCam
-
-
-im_height = 9528
-im_width = 13376
 
 with open(str(from_here('config.yaml')), 'r') as f:
     config_data = yaml.load(f, Loader=yaml.SafeLoader)
@@ -32,7 +27,7 @@ class CameraController():
         self.dirName = from_root(parent_dir, imgDir)
         self.create_img_dir = True
         self.cam_conn = False
-        self.camera_timer = []
+        # self.camera_timer = []
         SVCam.InitSDK()
 
     def start_camera(self):
@@ -40,30 +35,30 @@ class CameraController():
         self.cam_obj.deviceDiscovery()
         isConnected = self.cam_obj.connectCamera()
         if isConnected:
-            logging.info("Found a camera connection")
+            print("Found a camera connection")
             self.cam_conn = True
             isStreamOpen = self.cam_obj.openStream()
             if isStreamOpen:
-                logging.info("Opened a stream")
+                print("Opened a stream")
                 self.cam_obj.startAcquisition()
         else:
-            logging.info("No camera found")
+            print("No camera found")
 
     def stop_camera(self):
         try:
             self.cam_obj.stopAcquisition()
             self.cam_obj.disconnectCamera()
         except Exception as e:
-            logging.error(e)
+            print(e)
         finally:
             self.cam_conn = False
     
     # function for capturing a set of images and if successful, send a preview of the image captured
     def capture_images(self):
-        if self.camera_timer:
-            timer_obj = self.camera_timer.pop(0)
-            if timer_obj.is_alive():
-                timer_obj.cancel()
+        # if self.camera_timer:
+        #     timer_obj = self.camera_timer.pop(0)
+        #     if timer_obj.is_alive():
+        #         timer_obj.cancel()
         if not self.cam_conn:
             self.start_camera()
         missing_list = self.trigger_camera()
@@ -79,7 +74,6 @@ class CameraController():
             response = make_response(content, 417)
         return response
 
-
     # function for triggering the camera to take the images
     def trigger_camera(self):
         t_stamp = str(int(time.time()))
@@ -90,7 +84,7 @@ class CameraController():
             cv2.imwrite(f"DSC_{t_stamp}.tiff", self.img_array)
         except Exception as e:
             self.img_array = np.arrray([])
-            logging.error(e)
+            print(e)
             self.stop_camera()
         finally:
             missing_images = self.find_and_rename_files(t_stamp)
@@ -98,7 +92,6 @@ class CameraController():
 
     # function to check whether both the image files have been downloaded from camera, if yes then rename them appropriately
     def find_and_rename_files(self, time_stamp):
-        # missing_files = ["JPEG", "RAW"]
         missing_files = ["TIFF", "RAW", "BMP"]
         timeout_start = time.time()
         new_filename = f"{self.location}_{time_stamp}"
@@ -125,7 +118,6 @@ class CameraController():
                 elif not missing_files or (time.time()-timeout_start > 5):
                     return missing_files
 
-
     # funtion to move image files to day's image collection directory
     def move_files(self, file_name):
         if self.create_img_dir:
@@ -136,8 +128,7 @@ class CameraController():
         except:
             return
 
-
-    # function to find the latest jpeg file in the image directory
+    # function to find the latest preview image file in the image directory
     def find_latest_image(self):
         list_of_files = glob.glob(f'{self.dirName}/*.BMP')
         fileName = None
@@ -145,32 +136,9 @@ class CameraController():
             fileName = max(list_of_files, key=os.path.getctime)
         return fileName
 
-    # def create_preview_img(self):
-    #     if self.img_array.size != 0:
-    #         layer1 = self.img_array[:,:,0]
-    #         nparray = layer1.reshape((int(im_height*im_width*1.5))).astype(np.uint16)
-    #         nparray_16bit = np.empty(im_width*im_height, dtype=np.uint16)
-
-    #         nparray_16bit[::2] = (nparray[::3] << 4) | (nparray[1::3] & 0b00001111)
-    #         nparray_16bit[1::2] = nparray[2::3] | ((nparray[1::3] & 0b11110000) << 4)
-    #         nparray_unpacked = nparray_16bit.reshape((im_height, im_width))<<4
-
-    #         image_data = (nparray_unpacked).astype(np.float32)/65535.
-
-    #         colour_image_gamma_adjusted = demosaicing_CFA_Bayer_Malvar2004(image_data, "RGGB")
-    #         colour_image_gamma_adjusted[colour_image_gamma_adjusted<0] = 0
-    #         colour_image_gamma_adjusted[colour_image_gamma_adjusted>1] = 1
-                
-    #         demosaiced = cv2.cvtColor(colour_image_gamma_adjusted.astype(np.float32), cv2.COLOR_RGB2BGR)
-    #         img = (demosaiced*65535).astype(np.uint16)
-    #         return img
-    #     else:
-    #         return None
-
     # function to encode latest jpeg file
     def encode_latest_image(self):
         img_file = self.find_latest_image()
-        # image = self.create_preview_img()
         if img_file is not None:
             image = cv2.imread(img_file)
             preview = cv2.resize(image, None, fx = 0.1, fy = 0.1)
@@ -183,9 +151,9 @@ class CameraController():
                 response.status_code = 200
         else:
             response = make_response("No image file found!", 400)
-        new_timer = threading.Timer(60, self.stop_camera)
-        self.camera_timer.append(new_timer)
-        new_timer.start()
+        # new_timer = threading.Timer(60, self.stop_camera)
+        # self.camera_timer.append(new_timer)
+        # new_timer.start()
         threading.Thread(target=self.remove_bmp(img_file)).start()
         return response
     
